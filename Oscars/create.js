13c249744c60a85a05cc485215953402fafcb42d
João Costa <joaoarcosta@tecnico.ulisboa.pt>
2023-09-11 21:59:06 +0100

    feat: added histogram showing the budget over each year's oscar

--- Oscars/create.js
+++ Oscars/create.js
@@ -242,3 +242,91 @@ function createLineChart(data) {
     .attr("transform", "rotate(-90)")
     .text("Budget");
 }
+
+function createHistogram(data) {
+  // Select the #barChart element and append an SVG to it
+  const svg = d3
+    .select("#vis4")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  // Create x and y scales for the bar chart
+  const xScale = d3
+    .scaleBand()
+    .domain(data.map((d) => d.oscar_year))
+    .range([width, 0])
+    .padding(1/3);
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.budget)])
+    .range([height, 0]);
+
+  // Create a color scale for the bars based on the budget data
+  const colorScale = d3
+    .scaleSequential(d3.interpolateBlues)
+    .domain([d3.min(data, (d) => d.budget), d3.max(data, (d) => d.budget)]);
+
+  // Append and style the bars using the data and scales
+  svg
+    .selectAll(".bar")
+    .data(data, (d) => d.title)
+    .enter()
+    .append("rect")
+    .attr("class", "bar data")
+    .attr("x", (d) => xScale(d.oscar_year))
+    
+    .attr("y", -height)
+    .attr("height", (d) => (height - yScale(d.budget)))
+    .attr("transform", `scale(1, -1)`)
+    
+    .attr("width", xScale.bandwidth())
+    .attr("fill", (d) => colorScale(d.budget))
+    .attr("stroke", "black")
+    .on("mouseover", handleMouseOver)
+    .on("mouseout", handleMouseOut)
+    .append("title")
+    .text((d) => d.title);
+
+  // Append x and y axes to the chart
+  svg
+    .append("g")
+    .attr("class", "x-axis")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(xScale));
+
+  svg
+    .selectAll(".x-axis text")
+    .attr("transform", "rotate(-45)")
+    .style("text-anchor", "end")
+    .attr("dx", "-0.8em")
+    .attr("dy", "0.15em");
+  
+  svg
+    .append("g")
+    .attr("class", "y-axis")
+    .call(d3.axisLeft(yScale)
+    .tickFormat((d) => d3.format(".1f")(d / 1000000) + "M")
+    .tickSizeOuter(0));
+
+  // Append x and y axis labels
+  svg
+    .append("text")
+    .attr("class", "x-axis-label")
+    .attr("x", width / 2)
+    .attr("y", height + margin.top + 30)
+    .style("text-anchor", "middle")
+    .text("Oscar Year");
+
+    svg
+    .append("text")
+    .attr("class", "y-axis-label")
+    .attr("x", -height / 2)
+    .attr("y", -margin.left + 30)
+    .style("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .text("Budget");
+
+}
