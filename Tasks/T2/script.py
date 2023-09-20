4d6826bab59997a11f50aec60567c6c618a39cca
JarCosta <joaoarcosta@tecnico.ulisboa.pt>
2023-09-20 18:00:04 +0100

    feat: better sollution to Task2

--- Tasks/T2/script.py
+++ Tasks/T2/script.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pycountry
 import json
+import numpy as np
 
 # Read the csv file
 df = pd.read_csv('Income by Country.csv')
@@ -15,24 +16,14 @@ for i in df.values:
     info = i[-1] # Might be useless
     # print(country, values)
     
+    values = [int(j) for j in values if j != ".." and j != " "]
 
-    sum_int = 0
-    len = 0
-    for j in values:
-        try:
-            sum_int += int(j)
 
-            len += 1
-        except:
-            # print(country, j)
-            pass
-    
-    if(len > 0):
+    if(len(values) > 0):
         try:
             countries.append(pycountry.countries.get(name=country).alpha_3)
-            average_income.append(round(sum_int/len, 2))
-        except:
-            # print(country)
+            average_income.append(round(np.mean(values), 2))
+        except AttributeError:
             pass
 
 new_df = pd.DataFrame({"Country": countries, "Average Income": average_income})
